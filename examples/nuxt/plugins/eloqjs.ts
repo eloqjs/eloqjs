01489c0437d43fe1c4d70dc8cbce5b05b8d797d8
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-02-02 21:07:04 -0300

    demo(nuxt): rename `API` to `ModelAPIStatic`

--- examples/nuxt/plugins/eloqjs.ts
+++ examples/nuxt/plugins/eloqjs.ts
@@ -2,37 +2,37 @@
 import { Plugin } from '@nuxt/types'
 import ELOQJS, { Model } from '@eloqjs/core'
 
-import ELOQJSAPI, { AxiosHttpClient, API } from '@eloqjs/api'
+import ELOQJSAPI, { AxiosHttpClient, ModelAPIStatic } from '@eloqjs/api'
 
 declare module 'vue/types/vue' {
   // this.$myInjectedFunction inside Vue components
   interface Vue {
-    $api<M extends typeof Model>(model: M): API<M>
+    $api<M extends typeof Model>(model: M): ModelAPIStatic<M>
   }
 }
 
 declare module '@nuxt/types' {
   // nuxtContext.app.$myInjectedFunction inside asyncData, fetch, plugins, middleware, nuxtServerInit
   interface NuxtAppOptions {
-    $api<M extends typeof Model>(model: M): API<M>
+    $api<M extends typeof Model>(model: M): ModelAPIStatic<M>
   }
   // nuxtContext.$myInjectedFunction
   interface Context {
-    $api<M extends typeof Model>(model: M): API<M>
+    $api<M extends typeof Model>(model: M): ModelAPIStatic<M>
   }
 }
 
 declare module 'vuex/types/index' {
   // this.$myInjectedFunction inside Vuex stores
   interface Store<S> {
-    $api<M extends typeof Model>(model: M): API<M>
+    $api<M extends typeof Model>(model: M): ModelAPIStatic<M>
   }
 }
 
 const myPlugin: Plugin = ({ $axios }, inject) => {
   ELOQJS.use(ELOQJSAPI, { httpClient: new AxiosHttpClient($axios) })
 
-  inject('api', (model: typeof Model) => new API(model))
+  inject('api', (model: typeof Model) => new ModelAPIStatic(model))
 }
 
 export default myPlugin
