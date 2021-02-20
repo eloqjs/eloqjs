72b6d58b151ab561a9d49b399ed1cca3adfdfa61
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-02-20 00:09:00 -0300

    feat(core-support): add "is" utils
    Add utilities: `isString`, `isNumber`, `isBoolean`, `isObject`, `isArray`, `isNull`, `isUndefined`, `isNullish` and `isModel`

--- packages/core/src/support/Utils.ts
+++ packages/core/src/support/Utils.ts
@@ -1,3 +1,5 @@
+import { Model } from '../model/Model'
+
 export type Wrapped<T> = { data: T }
 
 /**
@@ -41,4 +43,75 @@ export function assert(
   if (!condition) {
     throw new Error(['[ELOQJS]'].concat(message).join(' '))
   }
+}
+
+/**
+ * Determines whether the given value is the type of `function`.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isFunction(value: unknown): value is Function {
+  return typeof value === 'function'
+}
+
+/**
+ * Determines whether the given value is the type of `object`.
+ */
+export function isObject(value: unknown): value is Record<string, unknown> {
+  return typeof value === 'object' && !isArray(value) && !isNull(value)
+}
+
+/**
+ * Determines whether the given value is the type of `array`.
+ */
+export function isArray(value: unknown): value is unknown[] {
+  return Array.isArray(value)
+}
+
+/**
+ * Determines whether the given value is the type of `string`.
+ */
+export function isString(value: unknown): value is string {
+  return typeof value === 'string'
+}
+
+/**
+ * Determines whether the given value is the type of `number`.
+ */
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number'
+}
+
+/**
+ * Determines whether the given value is the type of `boolean`.
+ */
+export function isBoolean(value: unknown): value is boolean {
+  return typeof value === 'boolean'
+}
+
+/**
+ * Determines whether the given value is the type of `null`.
+ */
+export function isNull(value: unknown): value is null {
+  return value === null
+}
+
+/**
+ * Determines whether the given value is the type of `undefined`.
+ */
+export function isUndefined(value: unknown): value is undefined {
+  return value === undefined
+}
+
+/**
+ * Determines whether the given value is the type of `undefined` or `null`.
+ */
+export function isNullish(value: unknown): value is undefined | null {
+  return isUndefined(value) || isNull(value)
+}
+
+/**
+ * Determines whether the given value is the instance of {@link Model}.
+ */
+export function isModel(value: unknown): value is Model {
+  return isObject(value) && value instanceof Model
 }
