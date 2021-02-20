ff7f1ec261d1b33b140f24feaad23e3cbc2a2107
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-02-20 00:19:10 -0300

    test(core-feature): add feature tests of Collection

---
+++ packages/core/test/feature/collections/Add.spec.ts
@@ -0,0 +1,33 @@
+import { Collection } from '../../../src/collection/Collection'
+import User from './dummy/models/User'
+
+describe('Feature – Collections – Add', () => {
+  it('should add model', () => {
+    const user = new User()
+    const collection = new Collection<User>()
+
+    collection.add(user)
+
+    expect(collection.models).toEqual([user])
+  })
+
+  it('should convert an object into model on add', () => {
+    const user = {
+      id: 1
+    }
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    collection.add(user)
+
+    expect(collection.models[0]).toBeInstanceOf(User)
+  })
+
+  it('should add model on initialize', () => {
+    const user = new User()
+    const collection = new Collection<User>([user])
+
+    expect(collection.models).toEqual([user])
+  })
+})
---
+++ packages/core/test/feature/collections/Remove.spec.ts
@@ -0,0 +1,48 @@
+import { Collection } from '../../../src/collection/Collection'
+import User from './dummy/models/User'
+
+describe('Feature – Collections – Remove', () => {
+  it('should remove model', () => {
+    const user = new User()
+    const collection = new Collection<User>([user])
+
+    collection.remove(user)
+
+    expect(collection.models).toEqual([])
+  })
+
+  it('should remove by passing an object with ID', () => {
+    const user = new User({ id: 1 })
+    const collection = new Collection<User>([user], {
+      model: User
+    })
+
+    collection.remove({ id: 1 })
+
+    expect(collection.models).toEqual([])
+  })
+
+  it('should remove by filtering', () => {
+    const user1 = new User({ id: 1 })
+    const user2 = new User({ id: 2 })
+    const user3 = new User({ id: 3 })
+    const collection = new Collection<User>([user1, user2, user3])
+
+    collection.remove((model) => model.$id && model.$id > 1)
+
+    expect(collection.models).toEqual([user1])
+  })
+
+  it('should remove multiple models', () => {
+    const user1 = new User({ id: 1 })
+    const user2 = new User({ id: 2 })
+    const user3 = new User({ id: 3 })
+    const collection = new Collection<User>([user1, user2, user3], {
+      model: User
+    })
+
+    collection.remove([user2, { id: 3 }])
+
+    expect(collection.models).toEqual([user1])
+  })
+})
