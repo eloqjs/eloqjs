7b9a4d20af8d643531da3819c3d5218ba5e66e67
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-03-04 16:15:47 -0300

    test(core-feature-collections): improve tests of `add` and `remove`

--- packages/core/test/feature/collections/Add.spec.ts
+++ packages/core/test/feature/collections/Add.spec.ts
@@ -1,34 +1,147 @@
 import { Collection } from '../../../src/collection/Collection'
-import { assertCollection, assertInstanceOf } from '../../Helpers'
+import { assertCollection, assertInstanceOf, assertModel } from '../../Helpers'
 import User from './dummy/models/User'
 
 describe('Feature – Collections – Add', () => {
-  it('should add model', () => {
+  it('should add a model', () => {
     const user = new User()
     const collection = new Collection<User>()
 
     collection.add(user)
 
+    expect(user.$collections).toEqual([collection])
     assertCollection(collection, [user])
   })
 
-  it('should convert an object into model on add', () => {
-    const user = {
-      id: 1
-    }
+  it('should add an array of models', () => {
+    const collection = new Collection<User>()
+
+    const user1 = new User()
+    const user2 = new User()
+
+    const added = collection.add([user1, user2])
+
+    expect(added).toEqual([user1, user2])
+
+    expect(user1.$collections).toEqual([collection])
+    expect(user2.$collections).toEqual([collection])
+
+    assertCollection(collection, [user1, user2])
+  })
+
+  it('should convert an object into a model on add', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const obj = { id: 1, name: 'Joe Doe' }
+    const user = collection.add(obj)
+
+    expect(user.$collections).toEqual([collection])
+
+    assertInstanceOf(collection, User)
+    assertCollection(collection, [user])
+
+    assertModel(user, obj)
+  })
+
+  it('should convert an array of objects into models on add', () => {
     const collection = new Collection<User>([], {
       model: User
     })
 
-    collection.add(user)
+    const obj1 = { id: 1, name: 'Joe Doe' }
+    const obj2 = { id: 2, name: 'John Doe' }
+
+    const added = collection.add([obj1, obj2])
+
+    const user1 = added[0]
+    const user2 = added[1]
+
+    assertModel(user1, obj1)
+    assertModel(user2, obj2)
+
+    expect(user1.$collections).toEqual([collection])
+    expect(user2.$collections).toEqual([collection])
 
     assertInstanceOf(collection, User)
+    assertCollection(collection, [user1, user2])
   })
 
-  it('should add model on initialize', () => {
+  it('should convert an object into a model when using an extended collection', () => {
+    class UserCollection extends Collection<User> {
+      static model = User
+    }
+
+    const obj = { id: 1, name: 'Joe Doe' }
+    const collection = new UserCollection()
+
+    const user = collection.add(obj)
+
+    expect(user.$collections).toEqual([collection])
+
+    assertCollection(collection, [user])
+    assertInstanceOf(collection, User)
+
+    assertModel(user, obj)
+  })
+
+  it('should add a model on initialize', () => {
     const user = new User()
     const collection = new Collection<User>([user])
 
+    expect(user.$collections).toEqual([collection])
     assertCollection(collection, [user])
+  })
+
+  it('should register the collection to the model on add', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user = collection.add({ id: 1, name: 'Joe Doe' })
+
+    expect(user.$collections).toEqual([collection])
+  })
+
+  it('should return one model if adding one', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    expect(typeof collection.add({})).toBe('object')
+    expect(collection.add({})).toBeInstanceOf(User)
+  })
+
+  it('should return an array of models when adding many', () => {
+    const collection = new Collection<User>()
+    const added = collection.add([new User(), new User()])
+
+    expect(Array.isArray(added)).toBeTruthy()
+    expect(added[0]).toBeInstanceOf(User)
+  })
+
+  it('should return an array of models when adding many plain objects', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const added = collection.add([
+      { id: 1, name: 'Joe Doe' },
+      { id: 2, name: 'John Doe' }
+    ])
+
+    expect(Array.isArray(added)).toBeTruthy()
+
+    expect(added[0]).toBeInstanceOf(User)
+    expect(added[1]).toBeInstanceOf(User)
+
+    expect(added[0].id).toBe(1)
+    expect(added[1].id).toBe(2)
+  })
+
+  it('should thrown an error when trying to add a non-object-like model', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    expect(() => new Collection().add(5)).toThrow(
+      'Expected a model, plain object, or array of either.'
+    )
   })
 })
--- packages/core/test/feature/collections/Remove.spec.ts
+++ packages/core/test/feature/collections/Remove.spec.ts
@@ -3,17 +3,18 @@ import { assertCollection } from '../../Helpers'
 import User from './dummy/models/User'
 
 describe('Feature – Collections – Remove', () => {
-  it('should remove model', () => {
+  it('should remove a model', () => {
     const user = new User()
     const collection = new Collection<User>([user])
 
     collection.remove(user)
 
+    expect(user.$collections).toEqual([])
     assertCollection(collection, [])
   })
 
   it('should remove by passing an object with ID', () => {
-    const user = new User({ id: 1 })
+    const user = new User({ id: 1, name: 'Joe Doe' })
     const collection = new Collection<User>([user], {
       model: User
     })
@@ -23,27 +24,172 @@ describe('Feature – Collections – Remove', () => {
     assertCollection(collection, [])
   })
 
-  it('should remove by filtering', () => {
-    const user1 = new User({ id: 1 })
-    const user2 = new User({ id: 2 })
-    const user3 = new User({ id: 3 })
-    const collection = new Collection<User>([user1, user2, user3])
+  it('should remove by predicate', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    collection.add({ id: 2, name: 'John Doe' })
+    collection.add({ id: 3, name: 'Alex Doe' })
 
     collection.remove((model) => model.$id && model.$id > 1)
 
     expect(collection.models).toEqual([user1])
   })
 
-  it('should remove multiple models', () => {
-    const user1 = new User({ id: 1 })
-    const user2 = new User({ id: 2 })
-    const user3 = new User({ id: 3 })
-    const collection = new Collection<User>([user1, user2, user3], {
+  it('should remove an array of multiple objects', () => {
+    const collection = new Collection<User>([], {
       model: User
     })
 
-    collection.remove([user2, { id: 3 }])
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    collection.add({ id: 2, name: 'John Doe' })
+    collection.add({ id: 3, name: 'Alex Doe' })
+
+    collection.remove([{ id: 2 }, { id: 3 }])
 
     assertCollection(collection, [user1])
+  })
+
+  it('should remove an array of multiple models', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    const user2 = collection.add({ id: 2, name: 'John Doe' })
+    const user3 = collection.add({ id: 3, name: 'Alex Doe' })
+
+    collection.remove([user2, user3])
+
+    assertCollection(collection, [user1])
+  })
+
+  it('should skip remove if the collection does not contain the model', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    const user2 = collection.add({ id: 2, name: 'John Doe' })
+    const user3 = new User({ id: 3, name: 'Alex Doe' })
+
+    collection.remove(user3)
+    assertCollection(collection, [user1, user2])
+  })
+
+  it('should unregister the collection from the model on remove', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user = collection.add({ id: 1, name: 'Joe Doe' })
+
+    collection.remove(user)
+
+    expect(user.$collections).toEqual([])
+  })
+
+  it('should return all models removed', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    const user2 = collection.add({ id: 2, name: 'John Doe' })
+    const user3 = collection.add({ id: 3, name: 'Alex Doe' })
+
+    const removed = collection.remove([user1, user2, user3])
+
+    expect(removed).toEqual([user1, user2, user3])
+  })
+
+  it('should return all models removed by object', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    const user2 = collection.add({ id: 2, name: 'John Doe' })
+    const user3 = collection.add({ id: 3, name: 'Alex Doe' })
+
+    const removed = collection.remove([{ id: 1 }, { id: 2 }, { id: 3 }])
+
+    expect(removed).toEqual([user1, user2, user3])
+  })
+
+  it('should return a single model when removing only one', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    collection.add({ id: 2, name: 'John Doe' })
+    collection.add({ id: 3, name: 'Alex Doe' })
+
+    const removed = collection.remove(user1)
+
+    expect(removed).toBe(user1)
+  })
+
+  it('should return a single model when removing only one by object', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    collection.add({ id: 2, name: 'John Doe' })
+    collection.add({ id: 3, name: 'Alex Doe' })
+
+    const removed = collection.remove({ id: 1 })
+
+    expect(removed).toBe(user1)
+  })
+
+  it('should return an array of models when removing by predicate', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1, name: 'Joe Doe' })
+    collection.add({ id: 2, name: 'John Doe' })
+    collection.add({ id: 3, name: 'Alex Doe' })
+
+    const removed = collection.remove((model) => {
+      return model.id === 1
+    })
+
+    expect(removed).toEqual([user1])
+  })
+
+  it('should only return models that were removed', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+    const user1 = collection.add({ id: 1 })
+    const user2 = new User({ id: 2 }) // Don't add to the collection
+    const user3 = collection.add({ id: 3 })
+
+    const removed = collection.remove([user1, user2, user3])
+
+    expect(removed).toEqual([user1, user3])
+  })
+
+  it('should throw an error if model is null', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    expect(() => new Collection().remove(null)).toThrow(
+      'Expected function, object, array, or model to remove.'
+    )
+  })
+
+  it('should throw an error if model is undefined', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    expect(() => new Collection().remove(undefined)).toThrow(
+      'Expected function, object, array, or model to remove.'
+    )
+  })
+
+  it('should throw an error if model is false', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    expect(() => new Collection().remove(false)).toThrow(
+      'Expected function, object, array, or model to remove.'
+    )
   })
 })
