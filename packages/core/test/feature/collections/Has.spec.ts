9a5a301fa5a395aecda7f871165d2f611649b179
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-03-10 16:04:52 -0300

    test(core-feature-collections): improve tests of `has`

--- packages/core/test/feature/collections/Has.spec.ts
+++ packages/core/test/feature/collections/Has.spec.ts
@@ -23,4 +23,25 @@ describe('Feature – Collections – Has', () => {
 
     expect(collection.has(user2)).toBeFalsy()
   })
+
+  it('should return true when all given model instances are in the collection', () => {
+    const user1 = new User()
+    const user2 = new User()
+    const collection = new Collection<User>()
+
+    collection.add(user1)
+    collection.add(user2)
+
+    expect(collection.has([user1, user2])).toBeTruthy()
+  })
+
+  it('should return false when not all given model instances are in the collection', () => {
+    const user1 = new User()
+    const user2 = new User()
+    const collection = new Collection<User>()
+
+    collection.add(user1)
+
+    expect(collection.has([user1, user2])).toBeFalsy()
+  })
 })
