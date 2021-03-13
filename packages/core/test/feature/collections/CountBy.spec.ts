8ad0bc0fefb7e582735dcacaf6e0fcc391b9123b
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-03-13 11:44:49 -0300

    test(core-feature-collections): add tests of `countBy`

---
+++ packages/core/test/feature/collections/CountBy.spec.ts
@@ -0,0 +1,25 @@
+import { Collection } from '../../../src/collection/Collection'
+import Product from './dummy/models/Product'
+
+describe('Feature – Collections – CountBy', () => {
+  it('should count occurrences based on the closure', () => {
+    const collection = new Collection<Product>([], {
+      model: Product
+    })
+
+    collection.add([
+      { id: 1, name: 'Desk', price: 200 },
+      { id: 2, name: 'Chair', price: 150 },
+      { id: 3, name: 'Bookcase', price: 150 },
+      { id: 4, name: 'Table', price: 150 },
+      { id: 5, name: 'Monitor', price: 200 }
+    ])
+
+    const counted = collection.countBy((model) => String(model.price))
+
+    expect(counted).toEqual({
+      '150': 3,
+      '200': 2
+    })
+  })
+})
