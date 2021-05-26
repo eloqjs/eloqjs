500a5eb9c7b251a1f774e680b6c1ee8bd5c58ef1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-05-26 11:28:15 -0300

    fix(api-httpclient): export HttpClientOptions

--- packages/api/src/httpclient/index.ts
+++ packages/api/src/httpclient/index.ts
@@ -4,6 +4,7 @@ export {
   AxiosHttpClientResponse
 } from './axios'
 export { HttpClient } from './HttpClient'
+export { HttpClientOptions } from './HttpClientOptions'
 export { HttpClientPromise } from './HttpClientPromise'
 export { HttpClientResponse } from './HttpClientResponse'
 export { Thenable } from './Thenable'
