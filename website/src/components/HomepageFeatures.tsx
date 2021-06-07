db944d913a19f15cc2c8939c6e89ea948ec5f008
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-06-07 08:00:06 -0300

    chore(website): typescript support

--- website/src/components/HomepageFeatures.tsx
+++ website/src/components/HomepageFeatures.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import clsx from 'clsx'
+import React from 'react'
+
+import styles from './HomepageFeatures.module.css'
 
 const FeatureList = [
   {
@@ -11,7 +12,7 @@ const FeatureList = [
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
-    ),
+    )
   },
   {
     title: 'Focus on What Matters',
@@ -21,7 +22,7 @@ const FeatureList = [
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
-    ),
+    )
   },
   {
     title: 'Powered by React',
@@ -31,11 +32,11 @@ const FeatureList = [
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
-    ),
-  },
-];
+    )
+  }
+]
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }): JSX.Element {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -46,10 +47,10 @@ function Feature({Svg, title, description}) {
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default function HomepageFeatures() {
+export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
@@ -60,5 +61,5 @@ export default function HomepageFeatures() {
         </div>
       </div>
     </section>
-  );
+  )
 }
