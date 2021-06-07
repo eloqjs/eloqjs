a51e8aa88d058fd79d03c85f516ce9abe84f269e
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-06-07 08:25:35 -0300

    chore(website): eslint and prettier

---
+++ website/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true // Enable JSX since we're using React
+    }
+  },
+  plugins: ['simple-import-sort', 'import'],
+  settings: {
+    react: {
+      version: 'detect' // Automatically detect the react version
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+}
