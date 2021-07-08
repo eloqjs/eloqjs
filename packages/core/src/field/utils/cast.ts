7ee68dde64799b9a297c465361bb68f63479e1e7
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-07-08 13:18:00 -0300

    fix(core-field): fix `resolveCast`

--- packages/core/src/field/utils/cast.ts
+++ packages/core/src/field/utils/cast.ts
@@ -1,11 +1,6 @@
 import { isArray, isFunction, isUndefined } from '../../support/Utils'
 
 export function resolveCast(key: string, type: any, cast: any): any {
-  // TODO: Improve `isUndefined` check to avoid duplicated code
-  if (isUndefined(cast)) {
-    return cast
-  }
-
   if (isArray(type) && !isFunction(cast)) {
     throw new Error(
       `Invalid cast for field "${key}": The cast must be a Function when multiple types are defined.`
@@ -16,7 +11,13 @@ export function resolveCast(key: string, type: any, cast: any): any {
     return (value: any) => castValue(type, value)
   }
 
-  return (value: any) => castValue(cast, value)
+  if (isUndefined(cast)) {
+    return cast
+  }
+
+  throw new Error(
+    `Invalid cast for field "${key}": The cast must match the field type.`
+  )
 }
 
 function castValue(cast: any, value: any) {
