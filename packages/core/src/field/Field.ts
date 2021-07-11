bed6a8dbcd67e1cb3252c9658a6e510053a5ccb9
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-07-10 21:22:45 -0300

    feat(core-field): add support to relationships

--- packages/core/src/field/Field.ts
+++ packages/core/src/field/Field.ts
@@ -1,3 +1,5 @@
+import { Model } from '../model/Model'
+import { RelationEnum } from '../relations/RelationEnum'
 import {
   capitalize,
   isNull,
@@ -9,20 +11,23 @@ import {
 import { resolveCast } from './utils/cast'
 import { resolveDefault } from './utils/default'
 import { resolveNullable } from './utils/nullable'
+import { resolveRelation, resolveRelationType } from './utils/relation'
 import { resolveRequired } from './utils/required'
 import { getName, resolveType, validateType } from './utils/type'
 import { resolveValidator } from './utils/validator'
 
 export class Field {
+  public model: typeof Model
   public key: string
   public type: any
+  public relation?: RelationEnum
   public required: boolean = false
   public nullable: boolean = false
   public default?: any
   public validator: (value: any) => boolean = () => true
   public cast: any
 
-  public constructor(key: string, field: any) {
+  public constructor(key: string, field: any, model: typeof Model) {
     if (!isString(key)) {
       throw new Error('The field "key" must be an string.')
     }
@@ -31,6 +36,7 @@ export class Field {
       throw new Error('The field is not defined.')
     }
 
+    this.model = model
     this.key = key
 
     this._boot(field)
@@ -43,11 +49,20 @@ export class Field {
     }
 
     this.type = resolveType(this.key, field.type)
+    this.relation = resolveRelationType(
+      this.key,
+      this.type,
+      field.relation as RelationEnum
+    )
     this.required = resolveRequired(field.required)
-    this.nullable = resolveNullable(field.nullable)
+    this.nullable = resolveNullable(field.nullable, field.relation)
     this.default = resolveDefault(this.key, this.type, field.default)
     this.validator = resolveValidator(this.key, field.validator, this.validator)
-    this.cast = resolveCast(this.key, this.type, field.cast)
+    this.cast = resolveCast(
+      this.key,
+      this.type,
+      this.relation ? true : field.cast
+    )
   }
 
   public validate(value: any): true {
@@ -60,12 +75,16 @@ export class Field {
       return true
     }
 
-    if (!validateType(value, this.type) && !(isNull(value) && this.nullable)) {
+    if (
+      !validateType(value, this.type, this.relation) &&
+      !(isNull(value) && this.nullable)
+    ) {
       const typeName = getName(this.type)
       const valueName =
         (value && value.constructor && value.constructor.name) ||
         (isNull(value) ? 'Null' : capitalize(typeof value))
 
+      // TODO: Improve error for relationship collections
       throw new Error(
         `Invalid field: type check failed for field "${this.key}". Expected "${typeName}", got "${valueName}".`
       )
@@ -80,16 +99,22 @@ export class Field {
     return true
   }
 
-  public resolveValue(value: unknown): any {
+  public make(value: unknown, model: Model): any {
     const nullish = this.nullable ? null : undefined
     const fallback = this.default ?? nullish
 
     value = !isUndefined(value) ? value : fallback
 
-    if (this.cast && !isNullish(value)) {
+    if (this.cast && !isNullish(value) && !this.relation) {
       value = this.cast(value)
     }
 
-    return this.validate(value) && value
+    const valid = this.validate(value)
+
+    if (this.relation) {
+      value = resolveRelation(model, this.type, this.relation, this.key, value)
+    }
+
+    return valid && value
   }
 }
--- packages/core/src/field/utils/type.ts
+++ packages/core/src/field/utils/type.ts
@@ -1,4 +1,11 @@
-import { isArray, isNullish } from '../../support/Utils'
+import { RelationEnum } from '../../relations/RelationEnum'
+import {
+  isArray,
+  isCollection,
+  isModelClass,
+  isNullish,
+  isPlainObject
+} from '../../support/Utils'
 
 export function resolveType(key: string, type: any): any {
   if (isNullish(type)) {
@@ -8,9 +15,27 @@ export function resolveType(key: string, type: any): any {
   return type
 }
 
-export function validateType(value: any, type: any): boolean {
+export function validateType(
+  value: any,
+  type: any,
+  relation?: RelationEnum
+): boolean {
   if (isNullish(value)) {
     return false
+  }
+
+  if (relation && isModelClass(type)) {
+    switch (relation) {
+      case RelationEnum.HAS_ONE: {
+        return value instanceof type || isPlainObject(value)
+      }
+      case RelationEnum.HAS_MANY: {
+        return isCollection(value) || isArray(value)
+      }
+      default: {
+        return false
+      }
+    }
   }
 
   if (isArray(type)) {
