409d617d86adf68ff469e273c36716f14f27be88
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-10-20 20:33:43 -0300

    refactor(core-collection): rename `_self` to `_constructor`

--- packages/core/src/collection/Collection.ts
+++ packages/core/src/collection/Collection.ts
@@ -157,7 +157,9 @@ export class Collection<M extends Model = Model> {
 
     // Objects should be converted to model instances first, then added.
     if (isPlainObject(model)) {
-      return this.add(this._self()._createModel<M>(model, this._options.model))
+      return this.add(
+        this._constructor()._createModel<M>(model, this._options.model)
+      )
     }
 
     // At this point, `model` should be an instance of Model.
@@ -1349,7 +1351,7 @@ export class Collection<M extends Model = Model> {
   /**
    * Get the constructor of this collection.
    */
-  private _self(): typeof Collection {
+  private _constructor(): typeof Collection {
     return this.constructor as typeof Collection
   }
 
