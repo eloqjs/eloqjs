532d958ae0e538fcd64949df7c5001b9074e3947
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-02-16 13:11:49 -0300

    test(core-collection): improve clone method

--- packages/core/test/feature/collections/Clone.spec.ts
+++ packages/core/test/feature/collections/Clone.spec.ts
@@ -1,4 +1,5 @@
-import { Collection } from '../../../src/collection/Collection'
+import { Collection, Relations } from '../../../src'
+import BaseModel from '../../dummy/models/BaseModel'
 import { assertCollection } from '../../Helpers'
 import User from './dummy/models/User'
 
@@ -23,7 +24,9 @@ describe('Feature – Collections – Clone', () => {
 
     const cloned = collection.clone()
 
-    assertCollection(cloned, added)
+    added.forEach((model, index) => {
+      expect(cloned.models[index]).toBe(model)
+    })
   })
 
   it('should preserve the options', () => {
@@ -68,5 +71,142 @@ describe('Feature – Collections – Clone', () => {
     const cloned = collection.clone()
 
     assertCollection(cloned, added)
+  })
+
+  it('should clone models when `deep` option is enabled', () => {
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    const added = collection.add([
+      { id: 1, name: 'Joe Doe' },
+      { id: 2, name: 'John Doe' },
+      { id: 3, name: 'Alex Doe' }
+    ])
+
+    const cloned = collection.clone({ deep: true })
+
+    added.forEach((user, index) => {
+      const clone = cloned.models[index]
+      expect(clone).not.toBe(user)
+      expect(clone.$serialize()).toEqual(user.$serialize())
+    })
+  })
+
+  it('should clone models and their relationships when `deep` option is enabled and `deepLevel` is 2', () => {
+    class User extends BaseModel {
+      static entity = 'users'
+
+      id!: number
+      name!: string
+      phone!: Relations.HasOne<Phone>
+      posts!: Relations.HasMany<Post>
+
+      static fields() {
+        return {
+          id: {
+            type: Number,
+            nullable: true
+          },
+          name: String,
+          phone: {
+            type: Phone,
+            relation: 'HasOne'
+          },
+          posts: {
+            type: Post,
+            relation: 'HasMany'
+          }
+        }
+      }
+    }
+
+    class Phone extends BaseModel {
+      static entity = 'phones'
+
+      id!: number
+      user_id!: number
+      number!: number
+
+      static fields() {
+        return {
+          id: {
+            type: Number,
+            nullable: true
+          },
+          user_id: {
+            type: Number,
+            nullable: true
+          },
+          number: {
+            type: Number,
+            default: 0
+          }
+        }
+      }
+    }
+
+    class Post extends BaseModel {
+      static entity = 'posts'
+
+      static primaryKey = 'slug'
+
+      id!: number
+      user_id!: number
+      title!: string
+
+      static fields() {
+        return {
+          id: {
+            type: Number,
+            nullable: true
+          },
+          user_id: {
+            type: Number,
+            nullable: true
+          },
+          slug: String,
+          title: String
+        }
+      }
+    }
+
+    const collection = new Collection<User>([], {
+      model: User
+    })
+
+    const added = collection.add([
+      {
+        id: 1,
+        name: 'John Doe',
+        phone: { id: 1, user_id: 1, number: 123456789 },
+        posts: [
+          {
+            id: 1,
+            user_id: 1,
+            slug: 'my-awesome-post',
+            title: 'My awesome post!'
+          },
+          { id: 2, user_id: 1, slug: 'my-super-post', title: 'My super post!' }
+        ]
+      }
+    ])
+
+    const cloned = collection.clone({ deep: true, deepLevel: 2 })
+
+    added.forEach((user, index) => {
+      const clone = cloned.models[index]
+
+      expect(clone).not.toBe(user)
+      expect(clone.phone.data).not.toBe(user.phone.data)
+      expect(clone.posts.data).not.toBe(user.posts.data)
+      expect(clone.phone.data!.$serialize()).toEqual(
+        user.phone.data!.$serialize()
+      )
+      expect(
+        clone.posts.data.models.map((model) => model.$serialize())
+      ).toEqual(user.posts.data.models.map((model) => model.$serialize()))
+      expect(clone.$serialize()).toEqual(user.$serialize())
+    })
   })
 })
