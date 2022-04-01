3d4f882c1643c0543ace1d50487e073d171d0802
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-04-01 00:25:47 -0300

    feat(core-model): improve type merging of `defineFields`

--- packages/core/src/model/DefineFields.ts
+++ packages/core/src/model/DefineFields.ts
@@ -1,8 +1,15 @@
+import { MergeObject } from '../types/Utilities'
 import { ModelFields } from './FieldTypes'
 
 export function defineFields<T extends ModelFields>(fields: T): T
-export function defineFields<T1 extends ModelFields, T2 extends ModelFields>(fields: T1, baseFields: T2): T1 & T2
-export function defineFields<T1 extends ModelFields, T2 extends ModelFields>(fields: T1, baseFields?: T2): T1 | (T1 & T2) {
+export function defineFields<T1 extends Partial<T2> & ModelFields, T2 extends ModelFields, R = MergeObject<T1, T2>>(
+  fields: T1,
+  baseFields: T2
+): { [K in keyof R]: R[K] }
+export function defineFields<T1 extends Partial<T2> & ModelFields, T2 extends ModelFields, R = MergeObject<T1, T2>>(
+  fields: T1,
+  baseFields?: T2
+): T1 | { [K in keyof R]: R[K] } {
   if (baseFields) {
     return {
       ...baseFields,
