2240959987165bfeb653f89e95e351d5da21c4d7
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-04-09 03:14:27 -0300

    fix(core): add `@experimental` modifier to `defineFields`

--- packages/core/src/model/define-fields.ts
+++ packages/core/src/model/define-fields.ts
@@ -1,6 +1,11 @@
 import { ModelFields, ResolveFields } from '../types'
 import { MergeObject } from '../types/utilities'
 
+/**
+ * Defines the fields on a model.
+ *
+ * @experimental
+ */
 export function defineFields<T extends ModelFields>(fields: T): T
 export function defineFields<T1 extends ResolveFields<T2> & ModelFields, T2 extends ModelFields>(
   fields: T1,
