4551fe50e1839da923c890c4ade365607c2d3d4a
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-07-01 17:52:58 -0300

    fix(api): remove export of core plugin interface

--- packages/api/src/plugins/use.ts
+++ packages/api/src/plugins/use.ts
@@ -1,5 +1,3 @@
-export { Plugin, PluginComponents, PluginOptions } from '@eloqjs/core'
-
 import { Collection, Model, Plugin, PluginComponents, PluginOptions, Relation } from '@eloqjs/core'
 
 export function use(plugin: Plugin, options: PluginOptions = {}): void {
